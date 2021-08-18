31d70cfc5c67ec1140f0a6a5c8f8798db2be3ca5
azaroth42 <azaroth42@gmail.com>
2021-08-18 09:02:10 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-linkedart-plugin',
-    version='0.3.0',
+    version='0.4.0',
     description='A MkDocs plugin',
     long_description='',
     keywords='mkdocs',
